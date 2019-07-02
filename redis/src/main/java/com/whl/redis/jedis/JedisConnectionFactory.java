4f622063832aa76af17ec0086085d0dc74ef9b72
wanghuailong <wanghuailong@ezhangyu.com>
2019-07-02 17:41:44 +0800

    增加redis工程

--- redis/src/main/java/com/whl/redis/jedis/JedisConnectionFactory.java
+++ redis/src/main/java/com/whl/redis/jedis/JedisConnectionFactory.java
@@ -1,5 +1,9 @@
 package com.whl.redis.jedis;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 /**
  * 〈〉
  *
@@ -8,4 +12,34 @@ package com.whl.redis.jedis;
  * @since 1.0.0
  */
 public class JedisConnectionFactory {
+
+    public static JedisPool jedisPool;
+
+    static {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPool = new JedisPool(jedisPoolConfig, "192.168.1.128", 6379, 5000, null);
+
+    }
+
+
+    public static Jedis newConnection() {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+
+        } catch (Exception e) {
+//            logger.error(e.getMessage(), e);
+            System.out.println("error:" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            //注意这里不是关闭连接，在JedisPool模式下，Jedis会被归还给资源池。
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        return jedis;
+    }
+
+
 }
